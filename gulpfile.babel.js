ae6a77c1b43c8aa343a40c09e26c984715ee1f25
John Kirkley <johnkirkley@Johns-MacBook-Pro-2.local>
2017-04-06 14:00:34 -0400

    added linting

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,7 @@ import http from 'http';
 import open from 'open';
 import lazypipe from 'lazypipe';
 import nodemon from 'nodemon';
+import runSequence from 'run-sequence';
 
 var plugins = gulpLoadPlugins();
 var config;
@@ -34,6 +35,11 @@ const paths = {
     dist: 'dist'
 };
 
+
+/********************
+ * Helper functions
+ ********************/
+
 function onServerLog(log) {
     console.log(plugins.util.colors.white('[') +
         plugins.util.colors.yellow('nodemon') +
@@ -41,9 +47,174 @@ function onServerLog(log) {
         log.message);
 }
 
-gulp.task('serve', () => {
+function checkAppReady(cb) {
+    var options = {
+        host: 'localhost',
+        port: config.port
+    };
+    http
+        .get(options, () => cb(true))
+        .on('error', () => cb(false));
+}
+
+// Call page until first success
+function whenServerReady(cb) {
+    var serverReady = false;
+    var appReadyInterval = setInterval(() =>
+        checkAppReady((ready) => {
+            if (!ready || serverReady) {
+                return;
+            }
+            clearInterval(appReadyInterval);
+            serverReady = true;
+            cb();
+        }),
+        100);
+}
+
+/********************
+ * Env
+ ********************/
+
+gulp.task('env:all', () => {
+    let localConfig;
+    try {
+        localConfig = require(`./${serverPath}/config/local.env`);
+    } catch (e) {
+        localConfig = {};
+    }
+    plugins.env({
+        vars: localConfig
+    });
+});
+gulp.task('env:test', () => {
+    plugins.env({
+        vars: {NODE_ENV: 'test'}
+    });
+});
+gulp.task('env:prod', () => {
+    plugins.env({
+        vars: {NODE_ENV: 'production'}
+    });
+});
+
+/********************
+ * Reusable pipelines
+ ********************/
+
+
+/* Linting */
+
+let lintServerScripts = lazypipe()
+    .pipe(plugins.eslint, `${serverPath}/.eslintrc`)
+    .pipe(plugins.eslint.format);
+
+let lintServerTestScripts = lazypipe()
+    .pipe(plugins.eslint, {
+        configFile: `${serverPath}/.eslintrc`,
+        envs: [
+            'node',
+            'es6',
+            'mocha'
+        ]
+    })
+    .pipe(plugins.eslint.format);
+
+/* Transpile */
+
+let transpileServer = lazypipe()
+    .pipe(plugins.sourcemaps.init)
+    .pipe(plugins.babel, {
+        plugins: [
+            'transform-class-properties',
+            'transform-runtime'
+        ]
+    })
+    .pipe(plugins.sourcemaps.write, '.');
+
+/* Testing */
+
+let mocha = lazypipe()
+    .pipe(plugins.mocha, {
+        reporter: 'spec',
+        timeout: 5000,
+        require: [
+            './mocha.conf'
+        ]
+    });
+
+let istanbul = lazypipe()
+    .pipe(plugins.istanbul.writeReports)
+    .pipe(plugins.istanbulEnforcer, {
+        thresholds: {
+            global: {
+                lines: 80,
+                statements: 80,
+                branches: 80,
+                functions: 80
+            }
+        },
+        coverageDirectory: './coverage',
+        rootDirectory : ''
+    });
+
+/* Gulp tasks */
+
+gulp.task('transpile:server', () => {
+    return gulp.src(_.union(paths.server.scripts, paths.server.json))
+        .pipe(transpileServer())
+        .pipe(gulp.dest(`${paths.dist}/${serverPath}`));
+});
+
+gulp.task('lint:scripts:server', () => {
+    return gulp.src(_.union(paths.server.scripts, _.map(paths.server.test, blob => '!' + blob)))
+        .pipe(lintServerScripts());
+});
+
+gulp.task('lint:scripts:serverTest', () => {
+    return gulp.src(paths.server.test)
+        .pipe(lintServerTestScripts());
+});
+
+gulp.task('lint:scripts', cb => runSequence(['lint:scripts:server'], cb));
+
+gulp.task('jscs', () => {
+  return gulp.src(_.union(paths.client.scripts, paths.server.scripts))
+      .pipe(plugins.jscs())
+      .pipe(plugins.jscs.reporter());
+});
+
+gulp.task('clean:tmp', () => del(['.tmp/**/*'], {dot: true}));
+
+gulp.task('watch', () => {
+    var testFiles = _.union(paths.server.test.unit, paths.server.test.integration);
+
+    plugins.watch(_.union(paths.server.scripts, testFiles))
+        .pipe(plugins.plumber())
+        .pipe(lintServerScripts());
+
+    plugins.watch(_.union(paths.server.test.unit, paths.server.test.integration))
+        .pipe(plugins.plumber())
+        .pipe(lintServerTestScripts());
+});
+
+gulp.task('start:server', () => {
     process.env.NODE_ENV = process.env.NODE_ENV || 'development';
     config = require(`./${serverPath}/config/environment`);
     nodemon(`-w ${serverPath} ${serverPath}`)
         .on('log', onServerLog);
+});
+
+gulp.task('serve', cb => {
+    runSequence(
+        [
+            'clean:tmp',
+            'lint:scripts',
+            'env:all'
+        ],
+        // 'webpack:dev',
+        ['start:server'],
+        'watch',
+        cb
+    );
 });
